f589e03d2df8bce62af0e7130c3b4afa655aa5d4
RICHARD WEAVER <you@example.com>
2022-12-11 23:19:41 +0100

    Added new flower image

--- src/data/flowers.ts
+++ src/data/flowers.ts
@@ -2,10 +2,12 @@ import flower1 from '../assets/flowers/1.png'
 import flower2 from '../assets/flowers/2.png'
 import flower3 from '../assets/flowers/3.png'
 import flower4 from '../assets/flowers/4.png'
+import flower5 from '../assets/flowers/portia-weiss.png'
+
 
 export const flowers = [
     {title: "1", imgSource: flower1, category: "items"},
     {title: "2", imgSource: flower2, category: "items"},
     {title: "3", imgSource: flower3, category: "items"},
     {title: "4", imgSource: flower4, category: "items"},
-]
+    {title: "portia-weiss", imgSource: flower5, category: "items"},]
